81add00794e693ab4a54eb44516d7eb73c28cd46
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-06 13:43:35 -0400

    all crud for feature model

--- routes/api/feature.js
+++ routes/api/feature.js
@@ -4,13 +4,13 @@ const featureController = require('../../controllers/featureController');
 //matches with /api/feature
 router.route('/')
     .get(featureController.findAll)
-    // .post(featureController.create);
+    .post(featureController.create);
 
 //matches with /api/feature/:id
-// router.route('/:id')
-//     .get(featureController.findById)
-//     .put(featureController.update)
-//     .delete(featureController.remove);
+router.route('/:id')
+    .get(featureController.findById)
+    .put(featureController.update)
+    .delete(featureController.remove);
 
 module.exports = router;
 
