66fda6db71ca7f4f9448ba2b8996992ca64f88fc
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-06 15:02:55 -0400

    created api util file for front end

---
+++ client/src/utils/API.js
@@ -0,0 +1,84 @@
+import axios from 'axios';
+
+
+export default {
+// feature
+getFeatures: function() {
+    return axios.get('/api/feature');
+},
+
+getFeature: function(id) {
+    return axios.get('/api/feature/' + id)
+},
+
+createFeature: function(featureData) {
+    return axios.post('/api/feature', featureData)
+},
+
+updateFeature: function(id, featureData) {
+    return axios.put('/api/feature/' + id, featureData)
+},
+
+deleteFeature: function(id) {
+    return axios.delete('/api/feature/' + id)
+},
+
+
+
+
+
+
+// feedback
+
+getFeedback: function(id) {
+    return axios.get('/api/feedback/' + id)
+},
+
+createFeedback: function(feedbackData) {
+    return axios.post('/api/feedback', feedbackData)
+},
+
+updateFeedback: function(id, feedbackData) {
+    return axios.put('/api/feedback/' + id, feedbackData)
+},
+
+deleteFeedback: function(id) {
+    return axios.delete('/api/feedback/' + id)
+},
+
+
+
+
+
+//user
+
+getUsers: function() {
+    return axios.get('/api/user');
+},
+
+getUser: function(id) {
+    return axios.get('/api/user/' + id)
+},
+
+createUser: function(userData) {
+    return axios.post('/api/user', userData)
+},
+
+updateUser: function(id, userData) {
+    return axios.put('/api/user/' + id, userData)
+},
+
+deleteUser: function(id) {
+    return axios.delete('/api/user/' + id)
+}
+
+
+
+//config
+
+
+
+
+
+
+}
