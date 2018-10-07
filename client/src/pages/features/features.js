f88183e63f5516e2f4a7484b6e4241e84c767752
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-06 20:50:51 -0400

    empty routes

---
+++ client/src/pages/features/features.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class features extends Component {
+    state = {
+        foo: 'bar'
+    };
+
+    render() {
+        return (
+            <div>
+                features component
+        </div>
+        )
+    }
+
+}
+
+export default features;
