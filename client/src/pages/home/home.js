33afc044300f312c3d2efd6a37a3dea32aa59c8d
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-08 22:30:06 -0400

    working home page

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
+import './home.css';
+import { Helmet } from 'react-helmet';
 
 
-class home extends Component{
+
+
+class home extends Component {
     state = {
-        foo:'bar'
+        foo: 'bar'
     };
 
     render() {
         return (
             <div>
-                Is this thing on
+                <Helmet>
+                    <style>{'body { background-color: #778899 ; }'}</style>
+                </Helmet>
+                <div className='container'>
+                    <div class="row">
+
+                        <div class="center-align mt40 card p20 grey lighten-2">
+                            <span class="">
+                                <i class="fas fa-retweet fa-8x black-text darken-2"></i>
+                            </span>
+                            <h2>LoopFeed</h2>
+                            <div class="section"></div>
+                            <div class="flow-text">Quickly provide feedback for new and exciting features</div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         )
     };
