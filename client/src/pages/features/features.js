546ac2f5a548b80109911a3c0a312ad344fbee6b
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-10 22:24:48 -0400

    dynamic feature list

--- client/src/pages/features/features.js
+++ client/src/pages/features/features.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import API from '../../utils/API';
+import { List, ListItem } from '../../components/list';
+import { Helmet } from 'react-helmet';
 
 class features extends Component {
     state = {
         features: [],
-        
+
     };
 
     componentDidMount() {
@@ -13,17 +15,44 @@ class features extends Component {
 
     getFeatures = () => {
         API.getFeatures()
-            .then(res => 
-                this.setState({features: res.data})
-                )
-                .catch(err => console.log(err));
+            .then(res =>
+                this.setState({ features: res.data })
+            )
+            .catch(err => console.log(err));
     }
+
+
+
+
 
     render() {
         return (
             <div>
-                features component
-        </div>
+                <Helmet>
+                    <style>{'body { background-color: #778899 ; }'}</style>
+                </Helmet>
+
+                
+                <div className='row'>
+                        <h4 className='center-align'>Features</h4>
+                    </div>
+                
+                <List>
+                    {this.state.features.map(feature => {
+                        return (
+                            <ListItem
+                                key={feature._id}
+                                title={feature.title}
+                                status={feature.status}
+                                createdAt={feature.createdAt}
+                                team={feature.team}
+                                id={feature.id}
+                            />
+                        )
+                    })
+                    }
+                </List>
+            </div>
         )
     }
 
