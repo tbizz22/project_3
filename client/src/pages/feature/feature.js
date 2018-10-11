3ce15f22d761c70f9d6c4f410b4a3404d224bb16
tyler bisbee <tyler.bisbee@gmail.com>
2018-10-10 23:13:42 -0400

    feature page ready for design

--- client/src/pages/feature/feature.js
+++ client/src/pages/feature/feature.js
@@ -1,17 +1,64 @@
 import React, { Component } from 'react';
+import API from '../../utils/API';
+
+import { Helmet } from 'react-helmet';
 
 class feature extends Component {
     state = {
-        foo: 'bar'
+        title: '',
+        comments: [],
+        status: '',
+        team: '',
+        image: '',
+        created: ''
     };
 
-    render() {
-        return (
-            <div>
-                feature component
-        </div>
-        )
+    componentDidMount() {
+        const featureId = this.props.match.params.id;
+        console.log(featureId)
+
+        this.getFeature(featureId);
+    };
+
+    getFeature = (id) => {
+        API.getFeature(id)
+            .then(res => {
+                const r = res.data;
+                this.setState({
+                    title: r.title,
+                    comments: [r.comments],
+                    status: r.status,
+                    team: r.team,
+                    image: r.image,
+                    created: r.createdAt
+                })
+            })
+            .catch(err => { console.log(err) });
+
     }
+
+
+
+
+
+
+
+render() {
+    return (
+        <div>
+            <Helmet>
+                <style>{'body { background-color: #778899 ; }'}</style>
+            </Helmet>
+
+
+            <div className='row'>
+                <h4 className='center-align'>Features</h4>
+            </div>
+
+
+        </div>
+    )
+}
 
 }
 
